ccd65c8e08ac752ef1fac15170a564dcd4a49194
ryanoverleese <ryan@acreinsights.com>
2026-05-18 14:05:34 -0500

    Fix repair button text

--- app/installer/InstallerApp.tsx
+++ app/installer/InstallerApp.tsx
@@ -1483,7 +1483,7 @@ function InstallScreen({ assignment: a, installer, onBack, onSuccess }: {
               <path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"/>
               <line x1="12" y1="9" x2="12" y2="13"/><line x1="12" y1="17" x2="12.01" y2="17"/>
             </svg>
-            Mark as installed, needs repair
+            Mark as installed, but needs repaired
           </button>
           <button
             className="af-btn af-btn--primary af-btn--xl af-btn--block"
